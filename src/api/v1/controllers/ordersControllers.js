16824fe55170974d3883abd1a9acafedbe356595
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-10 00:26:29 +0700

    feat(controllers): create ordersControllers.js

---
+++ src/api/v1/controllers/ordersControllers.js
@@ -0,0 +1,13 @@
+import Order from '../../models/Order.js';
+
+export const createOrder = async (req, res) => {};
+
+export const getAllOrders = async (req, res) => {};
+
+export const getOrderById = async (req, res) => {};
+
+export const getMyOrder = async (req, res) => {};
+
+export const updateOrder = async (req, res) => {};
+
+export const deleteOrder = async (req, res) => {};
