a1970beb2dd3f72fd9b1245bbc8557d881a02d88
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-11 01:01:09 +0700

    feat(model): create OrderSchema and OrderItemSchema

--- src/model/Order.js
+++ src/model/Order.js
@@ -1,5 +1,71 @@
 import { Schema, model } from "mongoose";
 
-const OrderSchema = new Schema({});
+const ObjectId = Schema.Types.ObjectId;
 
-export const Order = model("Order", OrderSchema);
+const OrderItemSchema = new Schema({
+  product_id: {
+    type: ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  price_option: {
+    type: String,
+    enum: ["option1", "option2", "option3"],
+  },
+  product_size: {
+    type: String,
+    enum: ["option1", "option2", "option3"],
+  },
+});
+
+const OrderSchema = new Schema({
+  order_id: {
+    type: ObjectId,
+    unique: true,
+    required: true,
+  },
+  user_id: {
+    type: ObjectId,
+    ref: "User",
+    required: true,
+  },
+  order_items: {
+    type: [OrderItemSchema],
+    required: true,
+  },
+  total_price: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Shipped", "Delivered", "Cancelled"],
+    required: true,
+  },
+  order_at: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+const Order = model("Order", OrderSchema);
+
+export {Order, OrderItemSchema };
+
