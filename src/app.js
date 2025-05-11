ad0687367f8752bd07baead76f23797bddd013a6
kungpiyaphon <93111308+kungpiyaphon@users.noreply.github.com>
2025-05-11 14:22:58 +0000

    style: auto format

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import { errorMiddleware } from './api/v1/middlewares/errorMiddleware.js';
 import { loggerMiddlewares } from './api/v1/middlewares/loggerMiddleware.js';
 
 import helmet from 'helmet';
-import { corsOptions } from './configs/cors.js';
 
 const app = express();
 
@@ -14,9 +13,9 @@ app.use(helmet());
 // app.use(cors(corsOptions));
 app.use(
   cors({
-    origin: "http://localhost:5173", // URL ของ Front-End ที่อนุญาตให้เข้าถึง
-    credentials: true,               // อนุญาตส่ง cookies, headers
-  })
+    origin: 'http://localhost:5173', // URL ของ Front-End ที่อนุญาตให้เข้าถึง
+    credentials: true, // อนุญาตส่ง cookies, headers
+  }),
 );
 app.use(json());
 app.use(loggerMiddlewares);
