07b321b90d3627af40b4651680c4513521ba7b4e
KUNG PIYAPHON <kungpiyaporn.0452@gmail.com>
2025-05-12 12:11:11 +0700

    feat(cors): updated cors app.js

--- src/app.js
+++ src/app.js
@@ -11,13 +11,7 @@ import { corsOptions } from './configs/cors.js';
 const app = express();
 
 app.use(helmet());
-// app.use(cors(corsOptions));
-app.use(
-  cors({
-    origin: "http://localhost:5173", // URL ของ Front-End ที่อนุญาตให้เข้าถึง
-    credentials: true,               // อนุญาตส่ง cookies, headers
-  })
-);
+app.use(cors(corsOptions));
 app.use(json());
 app.use(loggerMiddlewares);
 
