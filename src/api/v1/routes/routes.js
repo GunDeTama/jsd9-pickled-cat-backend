9a6c251794abee1fb13f02a5548a5671955b97a1
KUNG PIYAPHON <kungpiyaporn.0452@gmail.com>
2025-05-13 12:37:40 +0700

    feat(route): updated route order in app.js

--- src/api/v1/routes/routes.js
+++ src/api/v1/routes/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { productRoutes } from './productsRoutes.js';
 import { userRoutes } from './userRoutes.js';
+import { orderRoutes } from './orderRoutes.js';
 
 const router = Router();
 
 router.use('/api', userRoutes);
 router.use('/api', productRoutes);
+router.use('/api', orderRoutes);
 
 export { router as apiRoutes };
